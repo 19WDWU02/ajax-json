dfb4757efc07d1f793d5dbc0ad86207506302184
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-07-30 10:25:06 +1200

    retrives list of people using an ajax request

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  const btn = $('#getPeople');
+
+  btn.click(function(){
+    // console.log('button has been clicked');
+    $.ajax({
+      type: 'GET',
+      url: 'people.json',
+      dataType: 'json',
+      success: function(data){
+        console.log(data);
+        console.log('success');
+      },
+      error: function(error){
+        console.log(error);
+        console.log('error');
+      }
+    })
+
+
+  });
+
+
+
+});
