d3f1b0e8dfb2011714d81ea3100d227182d5eae1
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-07-31 10:48:46 +1200

    made request using json file on our project

--- js/script.js
+++ js/script.js
@@ -6,17 +6,29 @@ $(document).ready(function(){
     // console.log('button has been clicked');
     $.ajax({
       type: 'GET',
-      url: 'people.json',
+      url: 'data/MOCK_DATA.json',
       dataType: 'json',
       success: function(data){
         console.log(data);
         console.log('success');
+        data.map(function(person){
+          let card = '<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-1">';
+          card += '<div class="card text-center h-100">';
+          card += `<img src="${person['avatar']}" class="card-img-top" alt="">`;
+          card += '<div class="card-body">';
+          card += `<h5 class="card-title">${person['title']} ${person['first_name']} ${person['last_name']}</h5>`;
+          card += `<p class="card-text">${person['email']}</p>`;
+          card += '</div>';
+          card += '</div>';
+          card += '</div>';
+          $('#peopleContainer').append(card);
+        });
       },
       error: function(error){
         console.log(error);
         console.log('error');
       }
-    })
+    });
 
 
   });
